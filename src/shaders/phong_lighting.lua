a1b0e37ff2a19d7be2e14823317b75d4277158c4
Lloyd Lobo <lloydlobo4@gmail.com>
2024-11-02 12:23:15 +0530

    Normalize phong shader light shape from elliptical to circular

--- src/shaders/phong_lighting.lua
+++ src/shaders/phong_lighting.lua
@@ -5,38 +5,44 @@ local glsl_frag = [[
 #define NUM_LIGHTS 32
 
 struct Light{
-        vec2 position;
-        vec3 diffuse;
-        float power;
+     vec2 position;
+     vec3 diffuse;
+     float power;
 };
 
 extern Light lights[NUM_LIGHTS];
 extern int num_lights;
-
 extern vec2 screen;
 
 const float constant=1.;
-const float linear=.09;
-const float quadratic=.032;
+const float linear=.09;// Increase this to reduce light spread (e.g., try 0.2 or 0.3)
+const float quadratic=.032;// Increase this for a sharper falloff (e.g., 0.05 or 0.1)
+const float f_diffuse=.328;// Scale factor down final diffuse effect (orginally 1.)
 
 vec4 effect(vec4 color,Image image,vec2 uvs,vec2 screen_coords){
-        vec4 pixel=Texel(image,uvs);
+     vec4 pixel=Texel(image,uvs);
 
-        vec2 norm_screen=screen_coords/screen;
-        vec3 diffuse=vec3(0);
+     // Normalize screen coordinates
+     float aspect_ratio=screen.x/screen.y;
+     vec2 norm_screen=vec2(screen_coords.x/screen.x,screen_coords.y/screen.y);// Using `vec2` for dividing can be faster
+     vec2 orig_screen=norm_screen;// if we mutate norm_screen for effects
 
-        for(int i=0;i<num_lights;i++){
-                Light light=lights[i];
-                vec2 norm_position=light.position/screen;
+     vec3 col=vec3(0.);
 
-                float distance=length(norm_position-norm_screen)*light.power;
-                float attenuation=1./(constant+linear*distance+quadratic*(distance*distance));
-                diffuse+=light.diffuse*attenuation;
-        }
+     for(int i=0;i<num_lights;i++){
+          Light light=lights[i];
+          vec2 norm_pos=light.position/screen;
+          vec2 diff=vec2(
+               (norm_pos.x-norm_screen.x)*aspect_ratio,
+               norm_pos.y-norm_screen.y
+          );
+          float distance=length(diff)*light.power;
+          float attenuation=1./(constant+linear*distance+quadratic*(distance*distance));
+          col+=light.diffuse*attenuation*f_diffuse;
+     }
 
-        diffuse=clamp(diffuse,0.,1.);
-
-        return pixel*vec4(diffuse,1.);
+     col=clamp(col,0.,1.);
+     return pixel*vec4(col,1.);
 }
 ]]
 
